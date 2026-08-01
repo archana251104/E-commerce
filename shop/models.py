b045d8e4dd7161167a28934f98d7322b04fbfb74
archana251104 <archanaanand25114@gmail.com>
2026-08-01 12:31:28 +0530

    Update project

--- shop/models.py
+++ shop/models.py
@@ -5,6 +5,7 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
+    image = models.ImageField(upload_to='products/', blank=True, null=True) # <--- NEW LINE
     stock = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -26,7 +27,6 @@ class CartItem(models.Model):
     def total_price(self):
         return self.product.price * self.quantity
 
-# --- NEW ADDITIONS FOR CHECKOUT ---
 class Order(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
