211120394018ddbc8a6a39e2ce2f719667868b89
archana251104 <archanaanand25114@gmail.com>
2026-08-01 12:32:34 +0530

    Update project

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('shop.urls')),  # <--- This connects to shop/urls.py
+    path('', include('shop.urls')),
     path('accounts/', include('accounts.urls')),
 ]
+
+# This serves the images when DEBUG is True
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
