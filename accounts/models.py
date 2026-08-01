32b5fefc5438334148a3f2db8928268ea68b3151
archana251104 <archanaanand25114@gmail.com>
2026-08-01 12:51:51 +0530

    Update project

--- accounts/models.py
+++ accounts/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-    pass  # You can add custom fields here later
+    pass
 
     def __str__(self):
         return self.username
--- shop/models.py
+++ shop/models.py
@@ -3,12 +3,11 @@ from django.conf import settings
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
-    
     def __str__(self):
         return self.name
 
 class Product(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True) # <--- Added this line
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
