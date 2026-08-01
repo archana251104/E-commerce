f705860c8d8d0d001d07b1d50d131e3318141103
archana251104 <archanaanand25114@gmail.com>
2026-08-01 12:40:57 +0530

    Update project

--- shop/models.py
+++ shop/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 from django.conf import settings
 
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.name
+
 class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True) # <--- Added this line
     name = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
-    image = models.ImageField(upload_to='products/', blank=True, null=True) # <--- NEW LINE
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
     stock = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
 
