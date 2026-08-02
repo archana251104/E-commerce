b888fa401c3bd4f21a3d1135b77bda4431438c6a
archana251104 <archanaanand25114@gmail.com>
2026-08-02 10:12:42 +0530

    Update modified files

--- wishlist/views.py
+++ wishlist/views.py
@@ -9,38 +9,21 @@ from shop.models import Product
 @login_required
 def wishlist_view(request):
     wishlist, created = Wishlist.objects.get_or_create(user=request.user)
-    context = {
-        'wishlist': wishlist,
-        'wishlist_items': wishlist.items.select_related('product').all()
-    }
-    return render(request, 'wishlist/wishlist.html', context)
+    return render(request, 'wishlist/wishlist.html', {'wishlist': wishlist})
 
 @login_required
 @require_POST
 def toggle_wishlist(request):
     product_id = request.POST.get('product_id')
     product = get_object_or_404(Product, id=product_id)
-    
     wishlist, created = Wishlist.objects.get_or_create(user=request.user)
-    item = WishlistItem.objects.filter(wishlist=wishlist, product=product)
     
+    item = WishlistItem.objects.filter(wishlist=wishlist, product=product)
     if item.exists():
         item.delete()
-        product.wishlist_count -= 1
-        product.save()
-        action = 'removed'
-        message = f"{product.name} removed from wishlist"
+        messages.success(request, f"{product.name} removed from wishlist")
     else:
         WishlistItem.objects.create(wishlist=wishlist, product=product)
-        product.wishlist_count += 1
-        product.save()
-        action = 'added'
-        message = f"{product.name} added to wishlist"
+        messages.success(request, f"{product.name} added to wishlist")
     
-    if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
-        return JsonResponse({
-            'status': action,
-            'message': message,
-            'wishlist_count': product.wishlist_count
-        })
-    return redirect(request.META.get('HTTP_REFERER', 'shop:product_list'))
+    return redirect('shop:product_detail', product_id=product_id)
